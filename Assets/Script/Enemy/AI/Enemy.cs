8c909ec4dd578a62b5c08baaa9e520e6933cd2d8
SeokBeomKo <enngo98@gmail.com>
2022-07-28 21:48:02 +0900

    feat : 적 부모클래스 생성
    부모클래스 생성

---
+++ Assets/Script/Enemy/AI/Enemy.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct EnemyMaterial
+{
+    public SkinnedMeshRenderer meshRenderer;
+    public Color origin_1;
+    public Color origin_2;
+    public Color origin_3;
+}
+abstract public class Enemy : MonoBehaviour
+{
+    public Transform target;
+    public int maxHp;
+    public int curHp;
+    public float moveSpeed;
+    public float attackSpeed;
+    public int attack;
+    public int ignore;
+    public int defence;
+    public int expDrop;
+
+    private void OnEnable()
+    {
+        target = GameManager.instance.player.transform;
+        curHp = maxHp;
+    }
+
+    private void OnDisable()
+    {
+        ObjectPooler.ReturnToPool(gameObject);
+    }
+
+    abstract public void Attack();
+    abstract public void AttackReady();
+    abstract public void Tracing();
+}
